36bae1447b65a50d2a2e890db136f64c17509132
Avidor Turkewitz <aviturkewitz@gmail.com>
2016-02-18 16:57:16 -0500

    wrote rspecs and wrote merge sort to pass all rspecs

---
+++ merge_sort_5.rb
@@ -0,0 +1,25 @@
+def combine(array1, array2)
+  return array1 if array2 == nil
+  new_array = []
+  while array1.length > 0 && array2.length > 0 do
+    if array1.first > array2.first
+      new_array << array2.shift
+    else
+      new_array << array1.shift
+    end
+  end
+  new_array + array1 + array2
+end
+
+def sort(array)
+  new_array = []
+  while array.length >= 1 do
+    new_array << combine(array.shift, array.shift)
+  end
+  new_array.length == 1 ? new_array.flatten : sort(new_array)
+end
+
+def merge_sort(array)
+  split_array = array.collect{ |item| [item] }
+  sort(split_array)
+end
---
+++ spec/merge_sort_5_spec.rb
@@ -0,0 +1,25 @@
+require_relative 'spec_helper'
+
+describe 'combine' do
+  it 'takes two sorted arrays into one sorted array' do
+    expect(combine([1,3],[0,7])).to eq([0,1,3,7])
+  end
+
+  it 'takes in an array and a nil, and returns the first array' do
+    expect(combine([4,8,19],nil)).to eq([4,8,19])
+  end
+
+  it 'can combine arrays of different sizes' do
+    expect(combine([3,5,9,12],[1,7])).to eq([1,3,5,7,9,12])
+  end
+end
+
+describe 'merge_sort' do
+  let(:shuffle_array) {(0..57).to_a.shuffle}
+  let(:sorted_array) {(0..57).to_a}
+
+  it 'sort the array' do
+      expect(merge_sort(shuffle_array)).to eq(sorted_array)
+  end
+
+end
